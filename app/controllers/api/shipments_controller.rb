373980ed9ec4399e3129a40f769ee09c4bba805a
dschadd <dschadd@gmail.com>
2019-05-23 20:09:40 -0500

    add view and controller for shipments

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -1,2 +1,50 @@
 class Api::ShipmentsController < ApplicationController
+
+  def index
+    @shipments = Shipment.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    @shipment = Shipment.new(
+      description = params[:description],
+      price = params[:price],
+      weight = params[:weight],
+      length = params[:length],
+      width = params[:width],
+      height = params[:height]
+      )
+    if @shipment.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @shipment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @shipment = Shipment.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @shipment = Shipment.find_by(id: params[:id])
+    @shipment.description = params[:description] || @shipment.description
+    @shipment.price = params[:price] || @shipment.price
+    @shipment.weight = params[:weight] || @shipment.weight
+    @shipment.length = params[:length] || @shipment.length
+    @shipment.width = params[:width] || @shipment.width
+    @shipment.height = params[:height] || @shipment.height
+
+    if @shipment.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @shipment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shipment = Shipment.find_by(id: params[:id])
+    @shipment.destroy
+  end
+
 end
