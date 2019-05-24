7869a1d83a729f1a4acfbec027d4847bfcf923b1
dschadd <dschadd@gmail.com>
2019-05-23 20:31:11 -0500

    add controllers for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,26 @@
 class Api::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    # Use devise or JWT
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    # Find if there is a better way to update user
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy    
+  end
+
 end
