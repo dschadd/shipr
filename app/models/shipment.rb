a828ffa7655e8dbfbf4cc88b6bff8b0c9baeec36
dschadd <dschadd@gmail.com>
2019-06-13 18:22:03 -0500

    install figaro and ups api info

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,7 +2,7 @@ class Shipment < ApplicationRecord
   
   belongs_to :user
   belongs_to :courier
-  belongs_to :shipping address
+  belongs_to :shipping_address
   belongs_to :consignee
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-
-  has_many :shipments
-  has_many :shipping_addresses
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
