ee429b8f5f60b45c42348e39eba8abf342f27cee
dschadd <dschadd@gmail.com>
2019-07-08 19:15:17 -0500

    add rates controller

---
+++ app/controllers/api/rates_controller.rb
@@ -0,0 +1,156 @@
+class Api::RatesController < ApplicationController
+
+  def self.ups_rate_getter
+
+    headers = {
+      "Username" => "#{ENV["UPS_EMAIL"]}",
+      "Password" => "#{ENV["UPS_PASSWORD"]}", 
+      "AccessLicenseNumber" => "#{ENV["UPS_KEY"]}"
+    }
+    
+    json_data = { # Refactor this to declare outside of this HTTP request
+          "RateRequest": {
+            "Shipment": {
+              "Shipper": {
+                "Address": {
+                  "AddressLine": "569 Yarmouth Rd",
+                  "City": "Elk Grove Village",
+                  "StateProvinceCode": "IL",
+                  "PostalCode": "60007",
+                  "CountryCode": "US"
+                  }
+                },
+              "ShipTo": {
+                "Address":{
+                  "AddressLine": "500 Dogwood Trl",
+                  "City": "Elk Grove Village",
+                  "StateProvinceCode": "IL",
+                  "PostalCode": "60007",
+                  "CountryCode": "US"
+                }
+              },
+              "Package": {
+                "PackagingType": {
+                  "Code": "02"
+                },
+                "Dimensions": {
+                  "UnitOfMeasurement": {
+                    "Code": "IN"
+                  },
+                  "Length": "5",
+                  "Width": "4",
+                  "Height": "3"
+                },
+                "PackageWeight": {
+                  "UnitOfMeasurement": {
+                    "Code": "Lbs",
+                    "Description": "pounds"
+                  },
+                  "Weight": "1"
+                }
+              }
+            }
+          }
+        }
+
+    rate_response = HTTP
+      .headers(headers)
+      .post("https://wwwcie.ups.com/ship/v1801/rating/Shop", json: json_data)
+  end
+
+end
+
+
+# {
+#       "UPSSecurity": {
+#         "UsernameToken": {
+#           "Username": "Your User Id",
+#           "Password": "Your Password"
+#         },
+#         "ServiceAccessToken": {
+#           "AccessLicenseNumber": "Your Access License"
+#         }
+#       },
+#       "RateRequest": {
+#         "Request": {
+#           "RequestOption": "Rate",
+#           "TransactionReference": {
+#             "CustomerContext": "Your Customer Context"
+#           }
+#         },
+#         "Shipment": {
+#           "Shipper": {
+#             "Name": "Shipper Name",
+#             "ShipperNumber": "Shipper Number",
+#             "Address": {
+#               "AddressLine": [
+#                 "Address Line ",
+#                 "Address Line ",
+#                 "Address Line "
+#               ],
+#               "City": "City",
+#               "StateProvinceCode": "State Province Code",
+#               "PostalCode": "Postal Code",
+#               "CountryCode": "US"
+#             }
+#           },
+#           "ShipTo": {
+#             "Name": "Ship To Name",
+#             "Address": {
+#               "AddressLine": [
+#                 "Address Line ",
+#                 "Address Line ",
+#                 "Address Line "
+#               ],
+#               "City": "City",
+#               "StateProvinceCode": "State Province Code",
+#               "PostalCode": "Postal Code",
+#               "CountryCode": "US"
+#             }
+#           },
+#           "ShipFrom": {
+#             "Name": "Ship From Name",
+#             "Address": {
+#               "AddressLine": [
+#                 "Address Line ",
+#                 "Address Line ",
+#                 "Address Line "
+#               ],
+#               "City": "City",
+#               "StateProvinceCode": "State Province Code",
+#               "PostalCode": "Postal Code",
+#               "CountryCode": "US"
+#             }
+#           },
+#           "Service": {
+#             "Code": "03",
+#             "Description": "Service Code Description"
+#           },
+#           "Package": {
+#             "PackagingType": {
+#               "Code": "02",
+#               "Description": "Rate"
+#             },
+#             "Dimensions": {
+#               "UnitOfMeasurement": {
+#                 "Code": "IN",
+#                 "Description": "inches"
+#               },
+#               "Length": "5",
+#               "Width": "4",
+#               "Height": "3"
+#             },
+#             "PackageWeight": {
+#               "UnitOfMeasurement": {
+#                 "Code": "Lbs",
+#                 "Description": "pounds"
+#               },
+#               "Weight": "1"
+#             }
+#           },
+#           "ShipmentRatingOptions": {
+#             "NegotiatedRatesIndicator": ""
+#           }
+#         }
+#       }
+#     }
