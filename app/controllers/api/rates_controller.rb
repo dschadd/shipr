22d898042da9c4e9762a3c3a8c5ec1e1c6606ce0
dschadd <dschadd@gmail.com>
2019-07-18 18:35:49 -0500

    format http request to ups

--- app/controllers/api/rates_controller.rb
+++ app/controllers/api/rates_controller.rb
@@ -12,56 +12,102 @@ class Api::RatesController < ApplicationController
     ship_to_address = params[:ship_to_address]
     
     json_data = {
-      "RateRequest": {
-        "Shipment": {
-          "Shipper": {
-            "Address": {
-              "AddressLine": "#{shipper_address["address_line"]}",
-              "City": "#{shipper_address["city"]}",
-              "StateProvinceCode": "#{shipper_address["state_code"]}",
-              "PostalCode": "#{shipper_address["postal_code"]}",
-              "CountryCode": "#{shipper_address["country_code"]}"
-              }
-            },
-          "ShipTo": {
-            "Address":{
-              "AddressLine": "#{ship_to_address["address_line"]}",
-              "City": "#{ship_to_address["city"]}",
-              "StateProvinceCode": "#{ship_to_address["state_code"]}",
-              "PostalCode": "#{ship_to_address["postal_code"]}",
-              "CountryCode": "#{ship_to_address["country_code"]}"
-            }
-          },
-          "Package": {
-            "PackagingType": {
-              "Code": "02"
-            },
-            "Dimensions": {
-              "UnitOfMeasurement": {
-                "Code": "IN"
-              },
-              "Length": "5",
-              "Width": "4",
-              "Height": "3"
-            },
-            "PackageWeight": {
-              "UnitOfMeasurement": {
-                "Code": "Lbs",
-                "Description": "pounds"
-              },
-              "Weight": "1"
-            }
+  "RateRequest": {
+    "Shipment": {
+      "Shipper": {
+        "Address": {
+          "AddressLine": "569 Yarmouth Rd",
+          "City": "Elk Grove Village",
+          "StateProvinceCode": "IL",
+          "PostalCode": "60007",
+          "CountryCode": "US"
           }
+        },
+      "ShipTo": {
+        "Address":{
+          "AddressLine": "500 Dogwood Trl",
+          "City": "Elk Grove Village",
+          "StateProvinceCode": "IL",
+          "PostalCode": "60007",
+          "CountryCode": "US"
+        }
+      },
+      "Package": {
+        "PackagingType": {
+          "Code": "02"
+        },
+        "Dimensions": {
+          "UnitOfMeasurement": {
+            "Code": "IN"
+          },
+          "Length": "5",
+          "Width": "4",
+          "Height": "3"
+        },
+        "PackageWeight": {
+          "UnitOfMeasurement": {
+            "Code": "Lbs",
+            "Description": "pounds"
+          },
+          "Weight": "1"
         }
       }
     }
+  }
+}
 
-    rate_response = HTTP
+
+    # {
+    #   "RateRequest": {
+    #     "Shipment": {
+    #       "Shipper": {
+    #         "Address": {
+    #           "AddressLine": "#{shipper_address["address_line"]}",
+    #           "City": "#{shipper_address["city"]}",
+    #           "StateProvinceCode": "#{shipper_address["state_code"]}",
+    #           "PostalCode": "#{shipper_address["postal_code"]}",
+    #           "CountryCode": "#{shipper_address["country_code"]}"
+    #           }
+    #         },
+    #       "ShipTo": {
+    #         "Address":{
+    #           "AddressLine": "#{ship_to_address["address_line"]}",
+    #           "City": "#{ship_to_address["city"]}",
+    #           "StateProvinceCode": "#{ship_to_address["state_code"]}",
+    #           "PostalCode": "#{ship_to_address["postal_code"]}",
+    #           "CountryCode": "#{ship_to_address["country_code"]}"
+    #         }
+    #       },
+    #       "Package": {
+    #         "PackagingType": {
+    #           "Code": "02"
+    #         },
+    #         "Dimensions": {
+    #           "UnitOfMeasurement": {
+    #             "Code": "IN"
+    #           },
+    #           "Length": "5",
+    #           "Width": "4",
+    #           "Height": "3"
+    #         },
+    #         "PackageWeight": {
+    #           "UnitOfMeasurement": {
+    #             "Code": "Lbs",
+    #             "Description": "pounds"
+    #           },
+    #           "Weight": "1"
+    #         }
+    #       }
+    #     }
+    #   }
+    # }
+
+    @rate_response = HTTP
       .headers(headers)
       .post("https://wwwcie.ups.com/ship/v1801/rating/Shop",
-        :body => json_data)
+        :json => json_data)
 
-    render rate_response
+    render "show.json.jbuilder"
 
   end
 
---
+++ app/views/api/rates/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.ups @rate_response.body
