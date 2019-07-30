4828c5b1d5d5dacc6f3de272952ce19799d2947c
dschadd <dschadd@gmail.com>
2019-07-29 19:01:05 -0500

    add form for shipment booking

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -5,4 +5,84 @@ class Shipment < ApplicationRecord
   belongs_to :shipping_address
   belongs_to :consignee
 
+#   This is the JSON Body Form for booking a shipment.
+#   {
+#   "UPSSecurity": {
+#     "UsernameToken": {
+#       "Username": "dschadd",
+#       "Password": "grens111*"
+#     },
+#     "ServiceAccessToken": {
+#       "AccessLicenseNumber": "FD67EA90A84B5F15"
+#     }
+#   },
+#   "ShipmentRequest": {
+#     "Request": {
+#       "RequestOption": "validate",
+#       "TransactionReference": {
+#         "CustomerContext": "Your Customer Context"
+#       }
+#     },
+#     "Shipment": {
+#       "Shipper": {
+#         "Name": "Butch Schadd",
+#         "ShipperNumber": "43V647",
+#         "Address": {
+#           "AddressLine": "569 Yarmouth Rd",
+#           "City": "Elk Grove Village",
+#           "StateProvinceCode": "IL",
+#           "PostalCode": "60007",
+#           "CountryCode": "US"
+#           }
+#         },
+#       "ShipTo": {
+#         "Name": "Matt McCracken",
+#         "Address":{
+#           "AddressLine": "500 Dogwood Trl",
+#           "City": "Elk Grove Village",
+#           "StateProvinceCode": "IL",
+#           "PostalCode": "60007",
+#           "CountryCode": "US"
+#         }
+#       },
+#       "PaymentInformation": {
+#         "ShipmentCharge": {
+#           "Type": "01",
+#           "BillShipper": {
+#             "AccountNumber":
+#             "43V647"
+#           }
+#         }
+#       },
+#       "Service": {
+#         "Code": "01",
+#         "Description": "Express"
+#       },
+#       "Package": {
+#         "Description": "Description",
+#         "Packaging": { 
+#           "Code": "02",
+#           "Description": "Description"
+#         },
+#         "Dimensions": {
+#           "UnitOfMeasurement": {
+#             "Code": "IN",
+#             "Description": "Inches"
+#           },
+#           "Length": "7",
+#           "Width": "5",
+#           "Height": "2"
+#         },
+#         "PackageWeight": {
+#           "UnitOfMeasurement": {
+#             "Code": "LBS",
+#             "Description": "Pounds"
+#           },
+#           "Weight": "10"
+#         }
+#       }
+#     }
+#   }
+# }
+
 end
